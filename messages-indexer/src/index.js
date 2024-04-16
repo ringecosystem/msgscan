fd7b6981ba765405d5efaa512ca5487a05c0ecd3
Aki Wu <wuminzhe@gmail.com>
2024-04-16 16:15:43 +0800

    better main for messages indexer

--- messages-indexer/src/index.js
+++ messages-indexer/src/index.js
@@ -3,16 +3,30 @@ import { exit } from 'process';
 import { checkTableExists } from './db/prepare_tables.js'
 import syncMessages from './sync_messages.js'
 
-async function main() {
+async function start() {
   if (!await checkTableExists('indexer', 'Message')) { // schema from server/ponder.config.js -> database.publishSchema
-    console.error('indexer.Message table does not exist')
-    return
+    throw new Error('indexer.Message table does not exist')
   }
 
   const chainIds = [421614, 11155111, 167008]
   return Promise.all(chainIds.map(syncMessages))
 }
 
+async function main() {
+  try {
+    return await start()
+  } catch (error) {
+    await new Promise((resolve) => setTimeout(resolve, 10000)) // wait 10 seconds before retrying
+    throw error
+  }
+}
+
 main()
-  .then((_result) => exit(1)) // exit with 1 to indicate failure, this will restart the docker container
-  .catch((error) => { console.error(error); exit(1) })
+  .then((result) => {
+    console.log(result)
+    exit(0)
+  })
+  .catch((error) => {
+    console.error(error)
+    exit(1)
+  })
