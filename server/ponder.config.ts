91d56c264ad43d668a35d5eca0ce3eaa4d7e2d94
Aki Wu <wuminzhe@gmail.com>
2024-04-23 11:47:22 +0800

    update config

--- server/ponder.config.ts
+++ server/ponder.config.ts
@@ -17,11 +17,11 @@ export default createConfig({
       maxRequestsPerSecond: 5,
     },
     // testnets
-    // pangolin: {
-    //   chainId: 43,
-    //   transport: http(process.env.PONDER_RPC_URL_PANGOLIN),
-    //   maxRequestsPerSecond: 1,
-    // },
+    pangolin: {
+      chainId: 43,
+      transport: http(process.env.PONDER_RPC_URL_PANGOLIN),
+      maxRequestsPerSecond: 1,
+    },
     sepolia: {
       chainId: 11155111,
       transport: http(process.env.PONDER_RPC_URL_SEPOLIA),
@@ -32,11 +32,11 @@ export default createConfig({
       transport: http(process.env.PONDER_RPC_URL_ARBITRUM_SEPOLIA),
       maxRequestsPerSecond: 10,
     },
-    // takio_katla: {
-    //   chainId: 167008,
-    //   transport: http(process.env.PONDER_RPC_URL_TAKIO_KATLA),
-    //   maxRequestsPerSecond: 1,
-    // },
+    takio_katla: {
+      chainId: 167008,
+      transport: http(process.env.PONDER_RPC_URL_TAKIO_KATLA),
+      maxRequestsPerSecond: 1,
+    },
     // tron_shasta: {
     //   chainId: 2494104990,
     //   transport: http(process.env.PONDER_RPC_URL_TRON_SHASTA),
@@ -47,21 +47,21 @@ export default createConfig({
     // === V2
     ORMPV2: {
       abi: ORMPAbiV2,
-      address: "0x42165Ce95b51D1B845C190C96fB30c4FeF6Abce4",
+      address: "0xa30daF3c6071361960aF29e52C1eC860a037886f",
       network: {
         // testnets
-        // pangolin: {
-        //   startBlock: 2686802,
-        // },
+        pangolin: {
+          startBlock: 2701388,
+        },
         sepolia: {
-          startBlock: 5707420,
+          startBlock: 5728578,
         },
         arbitrum_sepolia: {
-          startBlock: 34110000,
+          startBlock: 35145606,
         },
-        // takio_katla: {
-        //   startBlock: 843300,
-        // },
+        taiko_katla: {
+          startBlock: 890471,
+        },
         // tron_shasta: {
         //   startBlock: 43245627,
         //   address: "0x4a7C839b0a32c90ad3b397875df73B905b1Bf0CA", // TGm4AeM42R9ocbbN3ibrDtf5kkQVTTFMYS
@@ -81,7 +81,7 @@ export default createConfig({
       address: "0xb2aa34fde97ffdb6197dd5a2be23c2121405cc12",
       network: {
         darwinia: {
-          startBlock: 2492391,
+          startBlock: 2513694,
         },
         // arbitrum_sepolia: {
         //   address: "0x2828c0223e6435365a3b977e87f12aefb7cbcbab",
