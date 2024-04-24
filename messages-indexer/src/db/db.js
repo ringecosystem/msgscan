bb407f50fd3828ba9ffdc637babb9f6e2f7e5616
Aki Wu <wuminzhe@gmail.com>
2024-04-24 11:58:39 +0800

    update docker-compose envs

--- messages-indexer/src/db/db.js
+++ messages-indexer/src/db/db.js
@@ -3,11 +3,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const sql = postgres({
-  host: process.env.PG_HOST || 'localhost',
-  username: process.env.PG_USERNAME || 'postgres',
-  password: process.env.PG_PASSWORD || 'password',
-  database: process.env.PG_DATABASE || 'postgres',
-  port: process.env.PG_PORT || 5432
+  host: process.env.POSTGRES_HOST || 'localhost',
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'password',
+  database: process.env.POSTGRES_DB || 'postgres',
+  port: process.env.POSTGRES_PORT || 5432
 });
 
 export default sql
