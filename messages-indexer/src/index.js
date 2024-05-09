f74692ec8ceb94654e7c46873cd1eb5c3ac75d30
Aki Wu <wuminzhe@gmail.com>
2024-05-09 13:36:16 +0800

    make main function better

--- messages-indexer/src/index.js
+++ messages-indexer/src/index.js
@@ -4,24 +4,26 @@ import { checkTableExists } from './db/prepare_tables.js'
 import syncMessages from './sync_messages.js'
 
 async function main() {
-  try {
-    if (!await checkTableExists('indexer', 'Message')) { // schema from server/ponder.config.js -> database.publishSchema
-      throw new Error("'indexer.Message' table does not exist. Please check if the server is running. It needs some time if it is the first time you are running the server.")
-    }
-
-    await syncMessages([421614, 11155111])
-  } catch (error) {
-    await new Promise((resolve) => setTimeout(resolve, 10000)) // wait 10 seconds before retrying by docker-compose
-    throw error
+  if (!await checkTableExists('indexer', 'Message')) { // schema from server/ponder.config.js -> database.publishSchema
+    throw new Error("Table 'indexer.Message' does not exist. Please check if the server is running. It needs some time if it is the first time you are running the server.")
   }
+
+  await syncMessages([421614, 11155111])
 }
 
-main()
-  .then((result) => {
-    console.log(result)
+(async () => {
+  try {
+    await main()
     exit(0)
-  })
-  .catch((error) => {
-    console.error(error)
+  } catch (error) {
+    if (error.message.includes("Table 'indexer.Message' does not exist.")) {
+      console.error(error.message)
+    } else {
+      console.error(error)
+    }
+
+    console.log('Waiting 10 minutes to exit.')
+    await new Promise((resolve) => setTimeout(resolve, 60 * 1000 * 10)) // wait 10 minutes
     exit(1)
-  })
+  }
+})()
