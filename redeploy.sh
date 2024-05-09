9eb60d106712ceb0b81d625cfc9dd84d823b6126
Aki Wu <wuminzhe@gmail.com>
2024-05-09 12:02:44 +0800

    update port

--- redeploy.sh
+++ redeploy.sh
@@ -3,7 +3,7 @@
 # Fully redeploy by cleaning up the `data` and `.ponder` directories
 
 docker compose down
-sudo rm -rf ~/msgscan/server/.ponder
-sudo rm -rf ~/msgscan/data
+sudo rm -rf ./server/.ponder
+sudo rm -rf ./data
 export $(grep -v '^#' .env | xargs) # Load environment variables
 docker compose up -d --build
