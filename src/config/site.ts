7e2c65e166b57f18654580cf2ca0875844646994
snoopy1412 <snoopy1412.su@gmail.com>
2024-07-03 13:57:45 +0800

    chore: Update site description in layout.tsx and site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,6 @@
 export const APP_NAME = 'Msgport scan';
-export const APP_DESCRIPTION = 'Msgport scan';
+export const APP_DESCRIPTION =
+  'The Msgport Scan is a one-stop solution for tracking the cross-chain message status and execution results for applications integrated with the Msgport protocol.';
 
 //interface 5000 ms
 export const REFRESH_INTERVAL = 5000;
